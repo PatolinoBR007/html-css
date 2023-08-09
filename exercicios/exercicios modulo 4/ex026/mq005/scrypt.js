b38019c25ca0160b8d5ce413db02952656a27f94
PatolinoBR007 <minehero023@gmail.com>
2023-08-09 14:05:08 -0300

    update mq005

--- exercicios/exercicios modulo 4/ex026/mq005/scrypt.js
+++ exercicios/exercicios modulo 4/ex026/mq005/scrypt.js
@@ -5,3 +5,10 @@ function clickMenu() {
         menu.style.display = 'block'
     }
 }
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
